cc77090b86b53aad5a632e33c36b2adfd7a219b0
Gonzalo Pizzolanti <gonzalopizzolanti.developer@gmail.com>
2025-05-09 23:27:31 +0200

    feat(player): increment default player velocity

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,7 @@
 class_name Player
 extends CharacterBody2D
 
-@export_enum("Slow:200", "Average:400", "Fast:600") var speed: int
+@export_enum("Slow:800", "Average:1000", "Fast:1200") var speed: int
 @onready var steerComponent: SteerComponent = $SteerComponent
 
 var is_hover: bool = false
@@ -11,29 +11,22 @@ func _process(_delta: float) -> void:
 	queue_redraw()
 
 func _physics_process(_delta):
-	# var direction: Vector2 = (get_global_mouse_position() - global_position)
-	# velocity = direction * 7.5
 	if !is_hover:
 		$Sprite.rotation = velocity.angle()
-	else:
-		$Sprite.rotation = 0
-		velocity = velocity.lerp(get_global_mouse_position(), _delta * 4)
-
-	velocity = steerComponent.steer_movement(
+		velocity = steerComponent.steer_movement(
 		velocity,
 		global_position,
 		get_global_mouse_position(),
 		mass,
-		speed,
-	)
-	
+		speed)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, 1)
 	move_and_slide()
 
 
-func _on_mouse_entered() -> void:
+func _on_area_2d_mouse_entered() -> void:
 	is_hover = true
-	print("ENTERED: ", is_hover)
 
-func _on_mouse_exited() -> void:
+
+func _on_area_2d_mouse_exited() -> void:
 	is_hover = false
-	print("LEAVE: ", is_hover)
