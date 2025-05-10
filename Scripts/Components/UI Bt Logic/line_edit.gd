ff8315058aa52615100f6ecbc1ffe718a5c47be8
Mario Viguer López <marioviguer@gmail.com>
2025-05-10 02:42:42 +0200

    Name Input Field Addition

---
+++ Scripts/Components/UI Bt Logic/line_edit.gd
@@ -0,0 +1,23 @@
+extends LineEdit
+@onready var info_panel: PanelContainer = $InfoPanel
+@onready var info: Label = $InfoPanel/Info
+
+func _on_text_submitted(new_text: String) -> void:
+	if (new_text != null and new_text != ""):
+		print("Cambiado")
+		PlayerName.username = new_text
+		clear()
+		placeholder_text = new_text
+		info_panel.visible = true; 
+		info.text = "¡Nombre cambiado!"
+		await get_tree().create_timer(5).timeout 
+		info_panel.visible = false; 
+	else:
+		print("NO cambiado")
+		info_panel.visible = true; 
+		info.text = "El nombre no es válido"
+		await get_tree().create_timer(5).timeout 
+		info_panel.visible = false; 
+		
+		
+	pass # Replace with function body.
