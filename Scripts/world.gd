db51e756e71a2959c5983144ad9dfc8e6005f728
Mario Viguer López <marioviguer@gmail.com>
2025-05-10 15:23:09 +0200

    Window Adjustments and UI improvements

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,2 +1,10 @@
 class_name World
 extends Node2D
+const MENU = preload("res://Scenes/intro_ui.tscn")
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ESCAPE:
+			print("Exit Menu")
+			get_tree().change_scene_to_packed(MENU)
+		
